9615a6bbc4502b0ca2640cfb4d9673963ce157f3
LuizBetaa <146309225+LuizBetaa@users.noreply.github.com>
2023-10-08 21:37:00 +0700

    Add files via upload

---
+++ lib/util/account_tab3.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class AccountTab3 extends StatelessWidget {
+  final List<String> userPosts = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: 5,
+      gridDelegate:
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.all(2.0),
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(
+                    'assets/waduh.jpeg',
+                  ),
+                  fit: BoxFit.fill),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
